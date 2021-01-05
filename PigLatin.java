a2f8d9d887f5416475d8013021b600f66988f1e4
Sadid-E <sethun20@stuy.edu>
2021-01-04 23:01:21 -0500

    Updated the pigLatinSimple method

--- PigLatin.java
+++ PigLatin.java
@@ -2,13 +2,26 @@ public class PigLatin {
 
   public static String pigLatinSimple(String s){
     s = s.toLowerCase();
-    if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' ||
-        s.charAt(0) == 'o' || s.charAt(0) == 'u') {
-          s = s + "hay";
-        } else {
-          s = s.substring(1,s.length()) + s.substring(0,1) + "ay";
-        }
+    String[] vowels = {"a","e","i","o","u"};
+    boolean a = false;
+    for (int i = 0; i < vowels.length; i++) {
+      if (vowels[i].equals(s.substring(0,1))) {
+          a = true;;
+      }
+    }
+    if (a) {
+      s = s + "hay";
+    } else {
+        s = s.substring(1,s.length()) + s.substring(0,1) + "ay";
+      }
     return s;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(pigLatinSimple("mock"));
+    System.out.println(pigLatinSimple("pie"));
+    System.out.println(pigLatinSimple("David"));
+    System.out.println(pigLatinSimple("Aaron"));
   }
 
 }
